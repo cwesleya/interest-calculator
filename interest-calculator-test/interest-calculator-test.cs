def2523994f49006f2df928cd841d4ae357837da
WAC0105 <wac0105@STL42WAC032022.local>
2020-06-23 23:47:25 -0500

    simplify test: only new up cards if needed; reuse if possible

--- interest-calculator-test/interest-calculator-test.cs
+++ interest-calculator-test/interest-calculator-test.cs
@@ -8,18 +8,19 @@ namespace interest_calculator_test
     [TestClass]
     public class InterestCalculatorUnitTests
     {
+        private Card _discoverCard = new Card("Discover", 100.0);
+        private Card _masterCard = new Card("Mastercard", 100.0);
+        private Card _visaCard = new Card("Visa", 100.0);
+
         [TestMethod]
+
         public void OnePerson_OneWallet_ThreeCards()
         {
-            var discoverCard = new Card("Discover", 100.0);
-            var masterCard = new Card("Mastercard", 100.0);
-            var visaCard = new Card("Visa", 100.0);
-
             var cards = new List<Card>
             {
-                discoverCard,
-                masterCard,
-                visaCard
+                _discoverCard,
+                _masterCard,
+                _visaCard
             };
 
             var wallet = new Wallet(cards);
@@ -30,20 +31,16 @@ namespace interest_calculator_test
             Assert.AreEqual(16.0, person.TotalInterest());
 
             //total interest per card
-            Assert.AreEqual(1.0, discoverCard.TotalInterest());
-            Assert.AreEqual(5.0, masterCard.TotalInterest());
-            Assert.AreEqual(10.0, visaCard.TotalInterest());
+            Assert.AreEqual(1.0, _discoverCard.TotalInterest());
+            Assert.AreEqual(5.0, _masterCard.TotalInterest());
+            Assert.AreEqual(10.0, _visaCard.TotalInterest());
         }
 
         [TestMethod]
         public void OnePerson_TwoWallets_ThreeCards()
         {
-            var discoverCard = new Card("Discover", 100);
-            var masterCard = new Card("Mastercard", 100);
-            var visaCard = new Card("Visa", 100);
-
-            var wallet1 = new Wallet(new List<Card> { discoverCard, visaCard });
-            var wallet2 = new Wallet(new List<Card> { masterCard });
+            var wallet1 = new Wallet(new List<Card> { _discoverCard, _visaCard });
+            var wallet2 = new Wallet(new List<Card> { _masterCard });
 
             var person = new Person(new List<Wallet> { wallet1, wallet2 });
 
@@ -58,14 +55,10 @@ namespace interest_calculator_test
         [TestMethod]
         public void TwoPersons_TwoWallets_FiveCards()
         {
-            var discoverCard = new Card("Discover", 100);
-            var masterCard1 = new Card("Mastercard", 100);
-            var visaCard1 = new Card("Visa", 100);
-
             var masterCard2 = new Card("Mastercard", 100);
             var visaCard2 = new Card("Visa", 100);
 
-            var wallet1 = new Wallet(new List<Card> { discoverCard, masterCard1, visaCard1 });
+            var wallet1 = new Wallet(new List<Card> { _discoverCard, _masterCard, _visaCard });
             var wallet2 = new Wallet(new List<Card> { masterCard2, visaCard2 });
 
             var person1 = new Person(new List<Wallet> { wallet1 });

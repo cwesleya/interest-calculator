69f76b5bd7bf0a3f3e998039264df239ad9acdbb
Wesley Chambers <c.wesleya@gmail.com>
2020-06-27 14:53:43 -0500

    organize

--- interest-calculator/Objects/Card.cs
+++ interest-calculator/Objects/Card.cs
@@ -5,27 +5,35 @@ namespace interest_calculator
 {
     public class Card : CardType, ITotalInterestCalculator
     {
+        private double _balance;
         private double? _totalInterest;
 
         public Card(string type, double balance)
         {
             _cardType = type;
             _balance = balance;
-            _rate = Enum.IsDefined(typeof(TypeName), type) ?
-                GetInterestRate(type) : 0.0;            
         }
 
         public double? TotalInterest()
         {
-            if (_totalInterest == null)
-                _totalInterest = CalculateInterest.Calculate(_rate, _balance);
-
+            if (_totalInterest == null)           
+                _totalInterest = CalculateInterest.Calculate(InterestRate(), _balance);
+            
             return _totalInterest;
         }
 
         public double Balance() => _balance;
 
-        public double InterestRate() => _rate;
+        public double InterestRate()
+        {
+            if (!TypeExists(_cardType))
+                return 0.0;
+
+            if (_rate == null)
+                _rate = InterestRate(_cardType);
+
+            return (double)_rate;
+        }
 
         public string CardType() => _cardType;
     }
---
+++ interest-calculator/Objects/CardType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace interest_calculator
+{
+    public abstract class CardType
+    {
+        protected string _cardType;
+        protected double? _rate;
+
+        protected double InterestRate(string type)
+        { 
+            return (double)(int)Enum.Parse(typeof(TypeName), type) / 100;
+        }
+
+        protected bool TypeExists(string type)
+        {
+            return Enum.IsDefined(typeof(TypeName), type);
+        }
+    }
+}
